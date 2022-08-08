d68110ff3d6caa49d0d08850ffc6e30f20f0d492
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-08-08 21:44:31 +0200

    basic template

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 module.exports = {
-    execute: function (message, args) {
-        message.channel.send(`Hi, ${message.author}. ${args[0] ? "Cool argument!" : "Try adding an argument!"}`)
-    },
-    registerEventHandlers: function (cb) {
-        cb("messageReactionAdd", async function (reaction, user){
-            if (reaction.partial) {
-                try {
-                    await reaction.fetch();
-                } catch (error) {
-                    console.error('Something went wrong when fetching the message:', error);
-                    return;
-                }
-            }        
-            reaction.message.channel.send("Someone reacted!") //please for the love of god dont do this in a real use case
-        })
+    api: function (somearguments) {
+        if(somearguments == "test") message.channel.send(`Hi, ${message.author}. ${args[0] ? args[0] : "Try adding an argument!"}`)
     }
 }
