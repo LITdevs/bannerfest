628a758f8510291fb3b7ec9cad299ad29b227306
Vukky <vukky@litdevs.org>
2022-08-09 16:53:07 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+let client;
+let config;
+const CronJob = require("cron").CronJob;
+
+function initAPIs(utils) {
+    config = util.apis["ccm-ccm"].config["bannerfest"];
+    client = utils.client
+    bannerify()
+    new CronJob('0 0 0 * * *', bannerify)
+}
+
+function bannerify() {
+    const availabanners = fs.readdirSync(`${__dirname}/../banners`).filter(fn => fn.startsWith(new Date().toLocaleString("en-GB", {day: "2-digit", month: "2-digit"}).replace("/", "-")));
+    console.log(availabanners)
+}
+
 module.exports = {
-    api: function (somearguments) {
-        if(somearguments == "test") message.channel.send(`Hi, ${message.author}. ${args[0] ? args[0] : "Try adding an argument!"}`)
-    }
+    initAPIs
 }
