df2254d7f9bde8f3d24e84001e5af7457d7a7649
Vukky <vukky@litdevs.org>
2022-08-09 17:23:39 +0200

    wooooooo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 let client;
 let config;
+let utilsy;
 var CronJob = require("cron").CronJob;
 const fs = require('fs');
 
 function initAPIs(utils) {
     config = utils.apis["ccm-ccm"].config["bannerfest"];
     client = utils.client
+    utilsy = utils
     bannerify()
     new CronJob('0 0 0 * * *', bannerify)
 }
 
 function bannerify() {
+    let decidedbanner;
     const availabanners = fs.readdirSync(`${__dirname}/../banners`).filter(fn => fn.startsWith(new Date().toLocaleString("en-GB", {day: "2-digit", month: "2-digit"}).replace("/", "-")));
-    console.log(availabanners)
+    if(availabanners[0]) {
+        if(!config.bannerState) {
+            config.bannerState = "holiday"
+            utilsy.apis["ccm-ccm"].api.save("bannerfest")
+        }
+        decidedbanner = availabanners[0]
+    } else if(config.bannerState == "holiday") {
+        config.bannerState = "normal"
+        utilsy.apis["ccm-ccm"].api.save("bannerfest")
+        decidedbanner = "00-00-missing.png"
+    }
+    client.guilds.fetch("868937321402204220").then(server => server.setBanner(`${__dirname}/../banners/${decidedbanner}`, "bannerfest update").catch(err => {
+        console.log(err)
+    }))
 }
 
 module.exports = {
