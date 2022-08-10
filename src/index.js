94c55fc73f08af9617f41e582694c3207b8ebac2
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-08-10 14:55:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ function bannerify() {
     const availabanners = fs.readdirSync(`${__dirname}/../banners`).filter(fn => fn.startsWith(new Date().toLocaleString("en-GB", {day: "2-digit", month: "2-digit"}).replace("/", "-")));
     if(availabanners[0]) {
         config.bannerState = "holiday"
-        utilsy.apis["ccm-ccm"].api.save("bannerfest")
+        utilsy.apis["ccm-ccm"].api.save("@litdevs/bannerfest")
         decidedbanner = availabanners[0]
     } else if(config.bannerState == "holiday") {
         config.bannerState = "normal"
-        utilsy.apis["ccm-ccm"].api.save("bannerfest")
+        utilsy.apis["ccm-ccm"].api.save("@litdevs/bannerfest")
         decidedbanner = "00-00-missing.png"
     }
     client.guilds.fetch("868937321402204220").then(server => server.setBanner(`${__dirname}/../banners/${decidedbanner}`, "bannerfest update")
